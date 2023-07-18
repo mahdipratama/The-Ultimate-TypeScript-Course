f434fb57df5094c0ebd4b3c7b1ed1c6d23294fd7
franki <mahdipratama29@gmail.com>
2023-07-18 15:54:14 +0700

    add Access Control keywords lecture

--- src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
+++ src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
@@ -53,3 +53,46 @@ class Account1 {
     this.balance += amount;
   }
 }
+
+//==================================================================//
+
+// Access Control Keywords
+
+/*
+Modifiers or controling access to properties and method to make more robust code 
+3 access modifiers in Typescript : 
+ - public
+ - private
+ - protected 
+*/
+
+class Account2 {
+  readonly id: number;
+  owner: string;
+  // private would make it not accessible
+  private _balance: number;
+  nickname?: string;
+
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this._balance = balance;
+  }
+
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error('Invalid amount');
+    this._balance += amount;
+  }
+
+  private calculateTax() {
+    return this._balance * 1.2;
+  }
+
+  getBalance(): number {
+    return this._balance;
+  }
+}
+
+const account2 = new Account2(1, 'Nami', 120);
+// We can't access the balance property outside of its class because we set the property with 'private' keyword
+// account2.balance = -1;
