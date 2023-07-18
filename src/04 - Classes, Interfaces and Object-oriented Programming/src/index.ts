d944335dd1c2c90e04eddf3a4fe445c6f4afb196
franki <mahdipratama29@gmail.com>
2023-07-18 15:09:29 +0700

    add Create Classes lecture

---
+++ src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
@@ -0,0 +1,16 @@
+class Account {
+  id: number;
+  owner: string;
+  balance: number;
+
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
+
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error('Invalid amount');
+    this.balance += amount;
+  }
+}
