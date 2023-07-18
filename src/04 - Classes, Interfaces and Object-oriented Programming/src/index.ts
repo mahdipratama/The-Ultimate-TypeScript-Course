55ac63b5da07a90dc4743385b6098789c6c06a15
franki <mahdipratama29@gmail.com>
2023-07-18 15:39:21 +0700

    add Read-only and Optional Properties lecture

--- src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
+++ src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
@@ -29,3 +29,27 @@ console.log(account1.balance); // 100
 console.log(account1); // Account { id: 1, owner: 'Nami', balance: 100 }
 console.log(typeof account1); // Object
 console.log(account1 instanceof Account); // true
+
+//==================================================================//
+
+// Read-only and Optional Properties
+
+class Account1 {
+  // read-only would make it robust
+  readonly id: number;
+  owner: string;
+  balance: number;
+  // '?' would make it Optional
+  nickname?: string;
+
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
+
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error('Invalid amount');
+    this.balance += amount;
+  }
+}
