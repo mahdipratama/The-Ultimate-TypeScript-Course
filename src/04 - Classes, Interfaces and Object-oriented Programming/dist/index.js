e7f8ddc7f38e4174ee9a31c546040af05d323672
franki <mahdipratama29@gmail.com>
2023-07-19 13:26:25 +0700

    [add] Method Overriding lecture

--- src/04 - Classes, Interfaces and Object-oriented Programming/dist/index.js
+++ src/04 - Classes, Interfaces and Object-oriented Programming/dist/index.js
@@ -84,15 +84,67 @@ seats.A1 = 'Nami';
 seats.A2 = 'Pika';
 class Ride {
     start() {
-        Ride.activeRides++;
+        Ride._activeRides++;
     }
     stop() {
-        Ride.activeRides--;
+        Ride._activeRides--;
+    }
+    static get activeRides() {
+        return Ride._activeRides;
     }
 }
-Ride.activeRides = 0;
+Ride._activeRides = 0;
 const ride1 = new Ride();
 ride1.start();
 const ride2 = new Ride();
 ride2.start();
 console.log(Ride.activeRides);
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    get fullName() {
+        return this.firstName + ' ' + this.lastName;
+    }
+    walk() {
+        console.log('Walking');
+    }
+}
+class Student extends Person {
+    constructor(studentId, firstName, lastName) {
+        super(firstName, lastName);
+        this.studentId = studentId;
+    }
+    takeTest() {
+        console.log('Taking a test');
+    }
+}
+class Person1 {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    get fullName() {
+        return this.firstName + ' ' + this.lastName;
+    }
+    walk() {
+        console.log('Walking');
+    }
+}
+class Student1 extends Person {
+    constructor(studentId, firstName, lastName) {
+        super(firstName, lastName);
+        this.studentId = studentId;
+    }
+    takeTest() {
+        console.log('Taking a test');
+    }
+}
+class Teacher extends Person1 {
+    get fullName() {
+        return 'Professor' + super.fullName;
+    }
+}
+let teacher = new Teacher('John', 'Smith');
+console.log(teacher.fullName);
--- src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
+++ src/04 - Classes, Interfaces and Object-oriented Programming/src/index.ts
@@ -244,3 +244,41 @@ class Student extends Person {
     console.log('Taking a test');
   }
 }
+
+//==================================================================//
+
+// Method Overriding
+// it means changing the implementation of the parent method
+
+class Person1 {
+  constructor(public firstName: string, public lastName: string) {}
+
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  walk() {
+    console.log('Walking');
+  }
+}
+
+class Student1 extends Person {
+  constructor(public studentId: number, firstName: string, lastName: string) {
+    super(firstName, lastName);
+  }
+
+  takeTest() {
+    console.log('Taking a test');
+  }
+}
+
+class Teacher extends Person1 {
+  // 'override' for changing the implementation of the method
+  // 'super.fullName' for reference the parent class (Person)
+  override get fullName() {
+    return 'Professor ' + super.fullName;
+  }
+}
+
+let teacher = new Teacher('John', 'Smith');
+console.log(teacher.fullName); // Professor John Smith
