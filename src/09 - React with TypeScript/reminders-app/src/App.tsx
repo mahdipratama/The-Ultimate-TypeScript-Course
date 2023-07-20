a80a14d47b0624284de1726435658443713cea09
franki <mahdipratama29@gmail.com>
2023-07-20 08:30:50 +0700

    fetch all the list from API

--- src/09 - React with TypeScript/reminders-app/src/App.tsx
+++ src/09 - React with TypeScript/reminders-app/src/App.tsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/reminder';
+import reminderService from './services/reminder';
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    { id: 1, title: 'Reminder 1' },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
 
   return (
     <div className="App">
