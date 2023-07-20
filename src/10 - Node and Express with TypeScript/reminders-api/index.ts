046d13f90ffaaea53d1e160098b4995f17f62345
franki <mahdipratama29@gmail.com>
2023-07-20 11:17:09 +0700

    [create] basic router

--- src/10 - Node and Express with TypeScript/reminders-api/index.ts
+++ src/10 - Node and Express with TypeScript/reminders-api/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import remindersRouter from './routers/reminder';
 const app = express();
+
+app.use('/reminders', remindersRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello world');
